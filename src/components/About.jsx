d94280fa249daead21e30eb508107a791637ffae
Matuwamai <wamaimatu@gmail.com>
2026-03-18 16:40:45 +0300

    Update profile photo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -127,7 +127,7 @@ const About = () => {
               <div className="relative">
                 <div className="w-64 h-64 mx-auto rounded-2xl overflow-hidden border-4 border-gradient-to-r from-blue-500 to-purple-500">
                   <img
-                    src="./public/matu.png"
+                    src="./matu.png"
                     alt="Profile"
                     className="w-full h-full object-cover, "
                   />
